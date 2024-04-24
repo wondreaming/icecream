d13161a9601a359e2f7d2ca9d68c2b9d046797b3
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-04-24 16:13:46 +0900

    Feat: go_router 부모, 자식 분기 설정

--- FE/icecream/lib/com/router/router.dart
+++ FE/icecream/lib/com/router/router.dart
@@ -1,15 +1,31 @@
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:icecream/com/widget/temp.dart';
-import 'package:icecream/main.dart';
+import 'package:icecream/home/screen/c_home.dart';
+import 'package:icecream/home/screen/p_home.dart';
 
 final router = GoRouter(
   routes: [
     GoRoute(
-      path: '/',
-      builder: (context, state) {
-        return Temp();
-      },
-    ),
+        path: '/',
+        builder: (context, state) {
+          return Temp();
+        },
+        routes: [
+          GoRoute(
+            path: 'parents',
+            builder: (context, state) {
+              return PHome();
+            },
+            routes: [
+
+            ]
+          ),
+          GoRoute(
+            path: 'children',
+            builder: (context, state) {
+              return CHome();
+            },
+          ),
+        ]),
   ],
 );
---
+++ FE/icecream/lib/setting/screen/setting.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:icecream/setting/widget/default_layout.dart';
+
+class Setting extends StatelessWidget {
+  const Setting({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(child: Center(child: Text('세팅'),));
+  }
+}
