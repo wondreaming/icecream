e476a322d2463277b4061c8c87fd47b8912c4134
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-04-24 14:09:44 +0900

    Rename : font, img 폴더 생성

---
+++ FE/icecream/lib/com/const/color.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// 초록
+const custom_green = Color(0xFF50D941);
+// 노랑
+const custom_yellow = Color(0xFFFFF6A6);
+// 빨강
+const custom_red = Color(0xFFF23D3D);
+// 글자 색상
+const custom_black = Color(0xFF5E5E5E);
+// 텍스트필드 배경 색상
+const custom_gray = Color(0xFFECECEC);
