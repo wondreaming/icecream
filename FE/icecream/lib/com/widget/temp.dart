c065748e34b187c65b7ebefa22b914f9d7afc41c
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-04-24 14:42:35 +0900

    Feat : 라이브러리 등록

---
+++ FE/icecream/lib/com/widget/temp.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Temp extends StatelessWidget {
+  const Temp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          '아이스크림',
+          style: TextStyle(fontFamily: 'omyuPretty'),
+        ),
+      ),
+    );
+  }
+}
