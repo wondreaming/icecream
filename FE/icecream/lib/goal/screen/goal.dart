21005b3735d0d39391e9719938b8a096736a920a
SeoJunHa96 <139415975+SeoJunHa96@users.noreply.github.com>
2024-04-24 18:04:40 +0900

    Feat: 네브바 구현중

---
+++ FE/icecream/lib/goal/screen/goal.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Goal extends StatelessWidget {
+  const Goal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Text('리워드 페이지.'));
+  }
+}
---
+++ FE/icecream/lib/noti/screen/notification.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Noti extends StatelessWidget {
+  const Noti({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Text('알림 페이지.'));
+  }
+}
