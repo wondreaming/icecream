e452d1240f8b91ae73a27940c1e05c53ff0b84ee
hluuy <dudwns1289@gmail.com>
2024-04-25 09:44:25 +0900

    Feat : Fcm 알림

--- FE/icecream/lib/main.dart
+++ FE/icecream/lib/main.dart
@@ -1,7 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'firebase_options.dart';
 import 'package:icecream/com/router/router.dart';
 
-void main() {
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  debugPrint(
+      "Handling a background message title: ${message.notification?.title}");
+  debugPrint(
+      "Handling a background message body: ${message.notification?.body}");
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  FirebaseMessaging firebaseMessaging = FirebaseMessaging.instance;
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  final fcmToken = await firebaseMessaging.getToken();
+  debugPrint('FCM Token: $fcmToken');
+
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+  NotificationSettings settings = await firebaseMessaging.requestPermission(
+    alert: true,
+    badge: true,
+    sound: true,
+    provisional: false,
+  );
+  debugPrint('User granted permission: ${settings.authorizationStatus}');
+
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('mipmap/ic_launcher');
+  const InitializationSettings initializationSettings =
+      InitializationSettings(android: initializationSettingsAndroid);
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+  // Notification Channel 설정
+  const AndroidNotificationChannel channel = AndroidNotificationChannel(
+      'high_importance_channel', 'High Importance Notifications',
+      description: 'This channel is used for important notifications.',
+      importance: Importance.high);
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  // Foreground 알림 메시지 처리
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
+    // 알림 세부정보
+    const NotificationDetails notificationDetails = NotificationDetails(
+        android: AndroidNotificationDetails(
+            'high_importance_channel', 'High Importance Notifications',
+            channelDescription:
+                'This channel is used for important notifications.',
+            importance: Importance.high,
+            priority: Priority.high,
+            icon: 'mipmap/ic_launcher',
+            ticker: 'ticker'));
+
+    // 알림 표시
+    await flutterLocalNotificationsPlugin.show(
+        message.hashCode,
+        message.notification?.title,
+        message.notification?.body,
+        notificationDetails,
+        payload: 'Notification Payload');
+
+    // 받은 알림 출력
+    debugPrint("Received notification title: ${message.notification?.title}");
+    debugPrint("Received notification: ${message.notification?.body}");
+  });
+
   runApp(const MyApp());
 }
 
