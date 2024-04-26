496871659a793389cb3e75450e7c2fe62dce881d
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-04-26 15:38:14 +0900

    Feat: option_menu 생성 완료

--- FE/icecream/lib/setting/screen/child_screen.dart
+++ FE/icecream/lib/setting/screen/child_screen.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:icecream/com/widget/default_layout.dart';
 import 'package:icecream/setting/widget/add_container.dart';
+import 'package:icecream/setting/widget/custom_elevated_button.dart';
+import 'package:icecream/setting/widget/custom_modal.dart';
 import 'package:icecream/setting/widget/custom_popupbutton.dart';
+import 'package:icecream/setting/widget/custom_show_dialog.dart';
+import 'package:icecream/setting/widget/custom_text_field.dart';
 import 'package:icecream/setting/widget/detail_profile.dart';
 
 class ChildScreen extends StatelessWidget {
@@ -17,63 +21,77 @@ class ChildScreen extends StatelessWidget {
           first: '이름 변경',
           secound: '전화번호 변경',
           third: '연결 해제',
-          firstOnTap: (){Future.delayed(
-            const Duration(seconds: 0),
-                () => showDialog(
-              context: context,
-              builder: (context) => const AlertDialog(
-                title: Text('test dialog'),
+          firstOnTap: () {
+            showCustomModal(
+              context,
+              '이름 변경',
+              Column(
+                children: [
+                  SizedBox(height: 16.0),
+                  CustomTextField(
+                    hintText: '변경할 이름을 입력해주세요',
+                  ),
+                  SizedBox(height: 16.0),
+                  CustomElevatedButton(onPressed: () {}, child: '저장'),
+                ],
               ),
-            ),
-          );},
-          secoundOnTap: (){},
-          thirdOnTap: (){},
+              160.0,
+            );
+          },
+          secoundOnTap: () {
+            // QR 찍는 페이지로 이동
+          },
+          thirdOnTap: () {
+            showCustomDialog(context, '연결 해제 하시겠습니까?');
+          },
         ),
       ],
-      child: Column(
-        children: [
-          DetailProfile(
-            is_parents: false,
-            id: '김자식',
-            number: '010-1234-5678',
-          ),
-          Container(
-            margin: EdgeInsets.only(bottom: 8.0, top: 16.0),
-            width: double.infinity,
-            height: 60,
-            padding: EdgeInsets.symmetric(horizontal: 15.0),
-            child: Row(
-              children: [
-                Flexible(
-                  child: Icon(
-                    Icons.place,
-                    size: 50,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            DetailProfile(
+              is_parents: false,
+              id: '김자식',
+              number: '010-1234-5678',
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 8.0, top: 16.0),
+              width: double.infinity,
+              height: 60,
+              padding: EdgeInsets.symmetric(horizontal: 15.0),
+              child: Row(
+                children: [
+                  Flexible(
+                    child: Icon(
+                      Icons.place,
+                      size: 50,
+                    ),
+                    flex: 1,
                   ),
-                  flex: 1,
-                ),
-                Flexible(
-                  child: Container(
-                    width: double.infinity,
-                    padding: EdgeInsets.only(left: 10.0),
-                    child: Text(
-                      '안심 보행',
-                      style: TextStyle(
-                        fontFamily: 'GmarketSans',
-                        fontSize: 26.0,
-                        fontWeight: FontWeight.w500,
+                  Flexible(
+                    child: Container(
+                      width: double.infinity,
+                      padding: EdgeInsets.only(left: 10.0),
+                      child: Text(
+                        '안심 보행',
+                        style: TextStyle(
+                          fontFamily: 'GmarketSans',
+                          fontSize: 26.0,
+                          fontWeight: FontWeight.w500,
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          AddContainer(
-              mention: '안심 보행지를 추가해주세요',
-              onPressed: () {
-                context.goNamed('location');
-              })
-        ],
+            AddContainer(
+                mention: '안심 보행지를 추가해주세요',
+                onPressed: () {
+                  context.goNamed('location');
+                })
+          ],
+        ),
       ),
     );
   }
