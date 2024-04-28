c9ae6c067832abe49139cdd0cf0708afe057692d
Jonggil-dev <jonggil.jeong24@gmail.com>
2024-04-28 23:57:03 +0900

    Feat : RabbitMQ 관련 GPS 수신 로직 추가

---
+++ BE/icecream/src/main/java/com/example/icecream/common/config/RabbitConfig.java
@@ -0,0 +1,10 @@
+package com.example.icecream.common.config;
+
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableRabbit
+public class RabbitConfig {
+
+}
