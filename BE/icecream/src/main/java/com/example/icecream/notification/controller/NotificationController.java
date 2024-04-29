ce632017aad00fb19000e6e05061eee72c4e5b43
심상익 <tkddlrtla@naver.com>
2024-04-29 12:33:23 +0900

    Feat : fcm 발송 및 발송내역 저장 기능

--- BE/icecream/src/main/java/com/example/icecream/notification/controller/NotificationController.java
+++ BE/icecream/src/main/java/com/example/icecream/notification/controller/NotificationController.java
@@ -1,17 +1,17 @@
 package com.example.icecream.notification.controller;
 
 import com.example.icecream.notification.dto.ApiResponseDto;
+import com.example.icecream.notification.dto.FcmRequestDto;
 import com.example.icecream.notification.dto.NotificationResponseDto;
 import com.example.icecream.notification.service.NotificationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class NotificationController {
 
     private final NotificationService notificationService;
@@ -21,8 +21,18 @@ public class NotificationController {
     public ApiResponseDto<List<NotificationResponseDto>> getNotificationList(@PathVariable int userId) {
         List<NotificationResponseDto> notificationList = notificationService.getNotificationList(userId);
         if (notificationList.isEmpty()) {
-            return ApiResponseDto.notFound();
+            return ApiResponseDto.notFound("알림 목록이 존재하지 않습니다.");
         }
-        return ApiResponseDto.success(notificationList);
+        return ApiResponseDto.success("알림 목록 조회에 성공하였습니다.", notificationList);
+    }
+
+    @PostMapping("/fcmTest")
+    public ApiResponseDto<String> fcmTest(@RequestBody FcmRequestDto fcmRequestDto) {
+        try {
+            notificationService.sendMessageTo(fcmRequestDto);
+        } catch (Exception e) {
+            return ApiResponseDto.error(e.getMessage());
+        }
+        return ApiResponseDto.success("메시지 전송 성공", null);
     }
 }
--- BE/icecream/src/main/java/com/example/icecream/notification/service/NotificationService.java
+++ BE/icecream/src/main/java/com/example/icecream/notification/service/NotificationService.java
@@ -2,9 +2,13 @@ package com.example.icecream.notification.service;
 
 import com.example.icecream.notification.document.FcmToken;
 import com.example.icecream.notification.document.NotificationList;
+import com.example.icecream.notification.dto.FcmMessageDto;
+import com.example.icecream.notification.dto.FcmRequestDto;
 import com.example.icecream.notification.dto.LoginRequestDto;
 import com.example.icecream.notification.dto.NotificationResponseDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface NotificationService {
@@ -13,4 +17,7 @@ public interface NotificationService {
     NotificationList saveNotificationList(Integer UserId, String content);
     List<NotificationResponseDto> getNotificationList(Integer UserId);
     // TODO: FCM 인증 및 발송 로직 추가 필요
+    void sendMessageTo(FcmRequestDto fcmRequestDto) throws IOException;
+    String getAccessToken() throws IOException;
+    String createMessage(FcmRequestDto fcmRequestDto) throws JsonProcessingException;
 }
