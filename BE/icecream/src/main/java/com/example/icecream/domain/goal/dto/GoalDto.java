6e09f14c2df5e5026e582e7f778a2526cd9de94b
jjin-21 <ljj1900@naver.com>
2024-05-01 20:04:43 +0900

    Add : 클래스 추가

---
+++ BE/icecream/src/main/java/com/example/icecream/domain/goal/dto/GoalDto.java
@@ -0,0 +1,11 @@
+package com.example.icecream.domain.goal.dto;
+
+import lombok.Getter;
+
+@Getter
+public class GoalDto {
+
+    private int userId;
+    private int period;
+    private String content;
+}
