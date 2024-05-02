0d1d3edb08c0f4b7e47657e3badb133f50e27453
Hwnag ChaeWon <155342577+wondreaming@users.noreply.github.com>
2024-05-02 17:51:28 +0900

    Feat: location 페이지 UI 완료

--- FE/icecream/lib/setting/widget/day_container.dart
+++ FE/icecream/lib/setting/widget/day_container.dart
@@ -1,30 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:icecream/com/const/color.dart';
 
-class DayContainer extends StatelessWidget {
+class DayContainer extends StatefulWidget {
   final String day;
-  const DayContainer({super.key, required this.day});
+  final int? isClicked;
+  const DayContainer({super.key, required this.day, this.isClicked});
 
   @override
+  State<DayContainer> createState() => _DayContainerState();
+}
+
+class _DayContainerState extends State<DayContainer> {
+  @override
   Widget build(BuildContext context) {
+    var isClicked;
     return Container(
-      child: Center(
-        child: Text(
-          day,
-          style: TextStyle(
-            fontFamily: 'GmarketSans',
-            fontSize: 20.0,
-            fontWeight: FontWeight.w400,
+          child: Center(
+            child: Text(
+              widget.day,
+              style: TextStyle(
+                fontFamily: 'GmarketSans',
+                fontSize: 20.0,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
           ),
-        ),
-      ),
-      height: 40.0,
-      width: MediaQuery.of(context).size.width * 1 / 5,
-      margin: EdgeInsets.symmetric(horizontal: 2.0),
-      decoration: BoxDecoration(
-        border: Border.all(color: AppColors.profile_black.withOpacity(0.5),),
-        borderRadius: BorderRadius.all(Radius.circular(8)),
-      ),
+          height: 40.0,
+          width: MediaQuery.of(context).size.width * 1 / 5,
+          margin: EdgeInsets.symmetric(horizontal: 2.0),
+          decoration: BoxDecoration(
+            color: (isClicked == 1) ? AppColors.custom_yellow.withOpacity(0.5) : AppColors.background_color,
+            border: Border.all(color: AppColors.profile_black.withOpacity(0.5),),
+            borderRadius: BorderRadius.all(Radius.circular(8)),
+          ),
     );
   }
 }
