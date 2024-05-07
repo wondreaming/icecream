2460056c057fca4cfb1953a13be486b23d2270c5
jjin-21 <ljj1900@naver.com>
2024-05-07 11:17:24 +0900

    Feat : ApiResponseDto

--- BE/icecream/src/main/java/com/example/icecream/domain/goal/controller/GoalController.java
+++ BE/icecream/src/main/java/com/example/icecream/domain/goal/controller/GoalController.java
@@ -8,6 +8,7 @@ import com.example.icecream.domain.goal.dto.UpdateGoalStatusDto;
 import com.example.icecream.domain.goal.entity.Goal;
 import com.example.icecream.domain.goal.service.GoalService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -21,25 +22,25 @@ public class GoalController {
     private final GoalService goalService;
 
     @PostMapping("/goal")
-    public ApiResponseDto<String> createGoal(@RequestBody CreateGoalDto createGoalDto) {
+    public ResponseEntity<ApiResponseDto<String>> createGoal(@RequestBody CreateGoalDto createGoalDto) {
         goalService.createGoal(createGoalDto);
         return ApiResponseDto.success("목표를 설정하였습니다.", null);
     }
 
     @PatchMapping("/goal")
-    public ApiResponseDto<String> updateGoal(@RequestBody UpdateGoalDto updateGoalDto) {
+    public ResponseEntity<ApiResponseDto<String>> updateGoal(@RequestBody UpdateGoalDto updateGoalDto) {
         goalService.updateGoal(updateGoalDto);
         return ApiResponseDto.success("목표를 수정하였습니다.", null);
     }
 
     @GetMapping("/goal")
-    public ApiResponseDto<List<Goal>> getGoal(@RequestParam(name = "user_id") int userId) {
+    public ResponseEntity<ApiResponseDto<List<Goal>>> getGoal(@RequestParam(name = "user_id") int userId) {
         List<Goal> goals = goalService.getGoals(userId);
         return ApiResponseDto.success("목표를 불러왔습니다.", goals);
     }
 
     @GetMapping("/goal/status")
-    public ApiResponseDto<Map<LocalDate, Integer>> getGoalStatus(
+    public ResponseEntity<ApiResponseDto<Map<LocalDate, Integer>>> getGoalStatus(
             @RequestParam("user_id") int userId,
             @RequestParam("date") LocalDate date) {
         GoalStatusDto goalStatus = goalService.getGoalStatus(userId, date);
@@ -47,7 +48,7 @@ public class GoalController {
     }
 
     @PatchMapping("/goal/status")
-    public ApiResponseDto<String> updateGoalStatus(@RequestBody UpdateGoalStatusDto updateGoalStatusDto) {
+    public ResponseEntity<ApiResponseDto<String>> updateGoalStatus(@RequestBody UpdateGoalStatusDto updateGoalStatusDto) {
         goalService.updateGoalStatus(updateGoalStatusDto);
         return ApiResponseDto.success("목표 상태를 수정하였습니다.", null);
     }
