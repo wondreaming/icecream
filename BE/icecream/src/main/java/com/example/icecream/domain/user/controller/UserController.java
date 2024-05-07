62eb54267c81c9b72a2e56400db22e8cead8aea6
Jonggil-dev <jonggil.jeong24@gmail.com>
2024-05-07 11:19:25 +0900

    Feat : 로그인 및 토큰 검증 로직 완료

--- BE/icecream/src/main/java/com/example/icecream/domain/user/controller/UserController.java
+++ BE/icecream/src/main/java/com/example/icecream/domain/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.icecream.domain.user.controller;
 
 import com.example.icecream.common.dto.ApiResponseDto;
+import com.example.icecream.domain.user.dto.CheckPasswordRequestDto;
 import com.example.icecream.domain.user.dto.SignUpChildRequestDto;
 import com.example.icecream.domain.user.dto.SignUpParentRequestDto;
 import com.example.icecream.domain.user.dto.UpdateChildRequestDto;
@@ -60,52 +61,9 @@ public class UserController {
         return ApiResponseDto.success("사용 가능한 ID 입니다.");
     }
 
-
+//    @PatchMapping("/password")
+//    public ResponseEntity<ApiResponseDto<String>> updatePassword(@RequestBody CheckPasswordRequestDto checkPasswordRequestDto, @AuthenticationPrincipal UserDetails userDetails) {
+//        userService.updatePassword(checkPasswordRequestDto.get,user.getUserId());
+//        return ApiResponseDto.success("비밀 번호가 확인되었습니다.");
+//    }
 }
-
-
-
-//    @GetMapping("/userInfo")
-//    public ResponseEntity<?> getInfo(@AuthenticationPrincipal UserDetails userDetails){
-//        User user = UserUtil.getUserFromUserDetails(userDetails);
-//        return buildBasicResponse(HttpStatus.OK,user.getUserId());
-//    }
-//
-//    @GetMapping("/check-nickname")
-//    public ResponseEntity<?> checkNickname(@RequestParam("userNickname") String userNickname) {
-//        userService.isValidateNickname(userNickname);
-//        return buildBasicResponse(HttpStatus.OK,"해당 닉네임은 사용 가능합니다.");
-//    }
-//
-//    @GetMapping("/check-loginId")
-//    public ResponseEntity<?> checkLoginId(@RequestParam("userLoginId") String userLoginId) {
-//        userService.isValidateLoginId(userLoginId);
-//        return buildBasicResponse(HttpStatus.OK,"해당 아이디는 사용 가능합니다.");
-//    }
-//
-//    @GetMapping("/update/check-nickname")
-//    public ResponseEntity<?> updateCheckNickname(@RequestParam("userNickname") String userNickname) {
-//        userService.isValidateNickname(userNickname);
-//        return buildBasicResponse(HttpStatus.OK,"해당 닉네임은 사용 가능합니다.");
-//    }
-//
-//    @GetMapping("/update/check-loginId")
-//    public ResponseEntity<?> updateCheckLoginId(@RequestParam("userLoginId") String userLoginId) {
-//        userService.isValidateLoginId(userLoginId);
-//        return buildBasicResponse(HttpStatus.OK,"해당 아이디는 사용 가능합니다.");
-//    }
-//
-//    @PatchMapping("/update/nickname")
-//    public ResponseEntity<?> updateNickname(@RequestBody UpdateNicknameRequest userNickname, @AuthenticationPrincipal UserDetails userDetails) {
-//        User user = UserUtil.getUserFromUserDetails(userDetails);
-//        userService.updateNickname(userNickname.getUserNickname(),user.getUserId());
-//        return buildBasicResponse(HttpStatus.OK,"닉네임 변경에 성공했습니다.");
-//    }
-//
-//    @PatchMapping("/update/password")
-//    public ResponseEntity<?> updatePassword(@RequestBody UpdatePasswordRequest userPassword, @AuthenticationPrincipal UserDetails userDetails) {
-//        User user = UserUtil.getUserFromUserDetails(userDetails);
-//        userService.updatePassword(userPassword,user.getUserId());
-//        return buildBasicResponse(HttpStatus.OK,"비밀번호 변경에 성공했습니다.");
-//    }
-//}
