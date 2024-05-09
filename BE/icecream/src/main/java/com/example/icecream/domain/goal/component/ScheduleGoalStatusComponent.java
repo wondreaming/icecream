3b4fc0bfe7a552d9b1cbc3b607bdc03012669b2a
심상익 <tkddlrtla@naver.com>
2024-05-09 17:48:42 +0900

    Feat : 목표 달성 시 목표 비활성화 및 알림 발송 기능, 목표 수정 예외 처리 추가

--- BE/icecream/src/main/java/com/example/icecream/domain/goal/component/ScheduleGoalStatusComponent.java
+++ BE/icecream/src/main/java/com/example/icecream/domain/goal/component/ScheduleGoalStatusComponent.java
@@ -1,8 +1,13 @@
 package com.example.icecream.domain.goal.component;
 
 import com.example.icecream.domain.goal.document.GoalStatus;
+import com.example.icecream.domain.goal.entity.Goal;
 import com.example.icecream.domain.goal.repository.mongodb.GoalStatusRepository;
+import com.example.icecream.domain.goal.repository.postgres.GoalRepository;
+import com.example.icecream.domain.notification.dto.FcmRequestDto2;
+import com.example.icecream.domain.notification.service.NotificationService;
 import com.example.icecream.domain.user.entity.User;
+import com.example.icecream.domain.user.repository.ParentChildMappingRepository;
 import com.example.icecream.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -13,6 +18,7 @@ import org.springframework.stereotype.Component;
 
 import java.time.Duration;
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -22,26 +28,40 @@ public class ScheduleGoalStatusComponent {
 
     private final UserRepository userRepository;
     private final GoalStatusRepository goalStatusRepository;
+    private final GoalRepository goalRepository;
+    private final ParentChildMappingRepository parentChildMappingRepository;
     private final RedisTemplate<String, Integer> redisTemplate;
+    private final NotificationService notificationService;
 
     @Scheduled(cron = "05 0 0 * * *")  // 초, 분, 시, 일, 월, 요일
     public void scheduleGoalStatusAndRedis() {
-        List<User> users = userRepository.findAll();
+        List<User> users = userRepository.findAllByIsParentAndIsDeletedFalse(false);
         ValueOperations<String, Integer> ops = redisTemplate.opsForValue();
 
         users.forEach(user -> {
-            if (!user.getIsParent()) {
             int userId = user.getId();
             GoalStatus goalStatus = goalStatusRepository.findByUserId(userId);
             Map<LocalDate, Integer> result = goalStatus.getResult();
             if (result.get(LocalDate.now().minusDays(1)) == 0) {
                 result.put(LocalDate.now().minusDays(1), 1);
                 goalStatusRepository.save(goalStatus);
+
+                Goal goal = goalRepository.findByUserIdAndIsActive(userId, true);
+                int record = goal.getRecord();
+                goal.updateRecord(record + 1);
+                if (record + 1 == goal.getPeriod()) {
+                    goal.deactivateGoal();
+
+                    int parentId = parentChildMappingRepository.findByChildId(userId).getParent().getId();
+                    List<Integer> userArray = Arrays.asList(userId, parentId);
+                    FcmRequestDto2 fcmRequestDto2 = new FcmRequestDto2(userArray, "목표 달성", user.getUsername() + "님, 목표를 달성하였습니다. 축하합니다!", "goal");
+                    notificationService.sendMessageToUsers(fcmRequestDto2);
+                }
+                goalRepository.save(goal);
             }
 
             String key = "user_goal:" + userId;
             ops.set(key, 0, Duration.ofSeconds(86400));
-            }
         });
     }
 }
