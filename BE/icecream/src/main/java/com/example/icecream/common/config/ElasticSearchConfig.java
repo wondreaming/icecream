b441af27c98412c1ef4db7ddf9e2d023ae9f7715
심상익 <tkddlrtla@naver.com>
2024-05-15 14:44:27 +0900

    Fix : elastic 연결 주석

--- BE/icecream/src/main/java/com/example/icecream/common/config/ElasticSearchConfig.java
+++ BE/icecream/src/main/java/com/example/icecream/common/config/ElasticSearchConfig.java
@@ -1,41 +1,41 @@
-package com.example.icecream.common.config;
-
-import org.elasticsearch.client.RestHighLevelClient;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.client.ClientConfiguration;
-import org.springframework.data.elasticsearch.client.RestClients;
-import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-
-@Configuration
-@EnableElasticsearchRepositories(basePackages = "com.example.icecream.domain.map.repository.elastic")
-public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
-
-    @Value("${ELASTICSEARCH_URL}")
-    private String Url;
-
-    @Value("${ELASTICSEARCH_USERNAME}")
-    private String Username;
-
-    @Value("${ELASTICSEARCH_PASSWORD}")
-    private String Password;
-
-    @Override
-    @Bean
-    public RestHighLevelClient elasticsearchClient() {
-        final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
-                .connectedTo(Url)
-                .withBasicAuth(Username, Password)
-                .build();
-        return RestClients.create(clientConfiguration).rest();
-    }
-
-    @Bean
-    public ElasticsearchOperations elasticsearchOperations() {
-        return new ElasticsearchRestTemplate(elasticsearchClient());
-    }
-}
+//package com.example.icecream.common.config;
+//
+//import org.elasticsearch.client.RestHighLevelClient;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.elasticsearch.client.ClientConfiguration;
+//import org.springframework.data.elasticsearch.client.RestClients;
+//import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+//import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+//import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+//import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+//
+//@Configuration
+//@EnableElasticsearchRepositories(basePackages = "com.example.icecream.domain.map.repository.elastic")
+//public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
+//
+//    @Value("${ELASTICSEARCH_URL}")
+//    private String Url;
+//
+//    @Value("${ELASTICSEARCH_USERNAME}")
+//    private String Username;
+//
+//    @Value("${ELASTICSEARCH_PASSWORD}")
+//    private String Password;
+//
+//    @Override
+//    @Bean
+//    public RestHighLevelClient elasticsearchClient() {
+//        final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+//                .connectedTo(Url)
+//                .withBasicAuth(Username, Password)
+//                .build();
+//        return RestClients.create(clientConfiguration).rest();
+//    }
+//
+//    @Bean
+//    public ElasticsearchOperations elasticsearchOperations() {
+//        return new ElasticsearchRestTemplate(elasticsearchClient());
+//    }
+//}
