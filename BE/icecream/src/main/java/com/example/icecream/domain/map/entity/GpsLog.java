6091a60c65a4118441a6ba70f1bd044b9bb19fcf
심상익 <tkddlrtla@naver.com>
2024-05-15 14:44:27 +0900

    Fix : elastic 연결 주석

--- BE/icecream/src/main/java/com/example/icecream/domain/map/entity/GpsLog.java
+++ BE/icecream/src/main/java/com/example/icecream/domain/map/entity/GpsLog.java
@@ -1,33 +1,33 @@
-package com.example.icecream.domain.map.entity;
-
-import lombok.Getter;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.elasticsearch.annotations.DateFormat;
-import org.springframework.data.elasticsearch.annotations.Document;
-import org.springframework.data.elasticsearch.annotations.Field;
-import org.springframework.data.elasticsearch.annotations.FieldType;
-
-import java.util.Date;
-
-@Getter
-@Document(indexName = "gps-logs-*")
-public class GpsLog {
-
-    @Id
-    private String id;
-
-    @Field(type = FieldType.Keyword)
-    private Integer userId;
-
-    @Field(type = FieldType.Double)
-    private Double latitude;
-
-    @Field(type = FieldType.Double)
-    private Double longitude;
-
-    @Field(type = FieldType.Keyword)
-    private String destinationId;
-
-    @Field(name = "@timestamp", type = FieldType.Date, format = DateFormat.date_time)
-    private Date timestamp;
-}
+//package com.example.icecream.domain.map.entity;
+//
+//import lombok.Getter;
+//import org.springframework.data.annotation.Id;
+//import org.springframework.data.elasticsearch.annotations.DateFormat;
+//import org.springframework.data.elasticsearch.annotations.Document;
+//import org.springframework.data.elasticsearch.annotations.Field;
+//import org.springframework.data.elasticsearch.annotations.FieldType;
+//
+//import java.util.Date;
+//
+//@Getter
+//@Document(indexName = "gps-logs-*")
+//public class GpsLog {
+//
+//    @Id
+//    private String id;
+//
+//    @Field(type = FieldType.Keyword)
+//    private Integer userId;
+//
+//    @Field(type = FieldType.Double)
+//    private Double latitude;
+//
+//    @Field(type = FieldType.Double)
+//    private Double longitude;
+//
+//    @Field(type = FieldType.Keyword)
+//    private String destinationId;
+//
+//    @Field(name = "@timestamp", type = FieldType.Date, format = DateFormat.date_time)
+//    private Date timestamp;
+//}
