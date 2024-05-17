1d01b2f00253f98384ae6528d6d3a30eb2216674
SeoJunHa96 <139415975+SeoJunHa96@users.noreply.github.com>
2024-05-17 15:32:38 +0900

    Design : 부모 CSS 수정

--- FE/icecream/lib/goal/screen/goal.dart
+++ FE/icecream/lib/goal/screen/goal.dart
@@ -6,7 +6,6 @@ import 'package:icecream/goal/model/goal_model.dart';
 import 'package:icecream/goal/widget/create_reward_modal.dart';
 import 'package:icecream/goal/widget/daily_goal.dart';
 import 'package:dropdown_button2/dropdown_button2.dart';
-import 'package:icecream/goal/widget/reward_modal.dart';
 import 'package:icecream/goal/service/goal_service.dart';
 import 'package:icecream/goal/widget/update_reward_modal.dart';
 
@@ -94,7 +93,7 @@ class _GoalState extends State<Goal> {
     return DefaultLayout(
       automaticallyImplyLeading: false,
       padding: EdgeInsets.zero,
-      title: '부모 리워드',
+      title: '안전 지킴이',
       child: Column(
         children: [
           const SizedBox(height: 25),
@@ -106,17 +105,18 @@ class _GoalState extends State<Goal> {
               children: [
                 RichText(
                   text: TextSpan(
-                    text: '현재 ',
-                    style: DefaultTextStyle.of(context).style,
+                    text: '지금까지 ',
+                    style: DefaultTextStyle.of(context).style.copyWith(fontFamily: 'GmarketSans', fontSize : 15),
                     children: <TextSpan>[
                       TextSpan(
                         text: '$currentStreak일째',
-                        style: const TextStyle(fontWeight: FontWeight.bold),
+                        style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 18),
                       ),
                       const TextSpan(text: ' 안전 보행 중입니다.'),
                     ],
                   ),
                 ),
+                SizedBox(height: 10,),
                 Expanded(
                   flex: 6, // 길이를 줄이기 위해 flex 비율 조정
                   child: DailyGoalPage(selectedChildId: int.parse(selectedChildId)), // String을 int로 변환
@@ -129,7 +129,7 @@ class _GoalState extends State<Goal> {
                       style: ElevatedButton.styleFrom(
                         backgroundColor: const Color.fromARGB(255, 25, 243, 116),
                       ),
-                      child: const Text('보상 수정하기'),
+                      child: const Text('보상 수정하기', style : TextStyle(fontFamily: 'GmarketSans')),
                     ),
                   ),
                 ),
@@ -150,40 +150,49 @@ class _GoalState extends State<Goal> {
 
   Widget buildChildDropdown() {
     return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.baseline,  // 교차 축 정렬을 기준선으로 변경
+      textBaseline: TextBaseline.alphabetic,  // 텍스트 기준선 설정
       children: [
         const SizedBox(width: 10),
-        SizedBox(
-          width: MediaQuery.of(context).size.width * 0.3,
-          child: Align(
-            alignment: Alignment.topLeft,
-            child: DropdownButtonFormField2(
-              decoration: InputDecoration(
-                contentPadding: const EdgeInsets.symmetric(vertical: 10),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15),
-                ),
+        Text('현재 자녀  : ', style: TextStyle(fontSize: 16, fontFamily: 'GmarketSans', height: 1.0)),
+        const SizedBox(width: 10),
+        Expanded(  // Flexible에서 Expanded로 변경하여 남은 공간을 모두 사용하도록 설정
+          child: DropdownButtonFormField2(
+            decoration: InputDecoration(
+              contentPadding: const EdgeInsets.symmetric(vertical: 10),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
               ),
-              isExpanded: true,
-              hint: const Text('자녀', style: TextStyle(fontSize: 14)),
-              value: selectedChild,
-              items: childrenItems
-                  .map((item) => DropdownMenuItem<String>(
-                value: item,
-                child: Text(item, style: const TextStyle(fontSize: 14)),
-              ))
-                  .toList(),
-              onChanged: (value) {
-                Child selected =
-                childrenData.firstWhere((child) => child.username == value);
-                updateChildSelection(selected);
-              },
+            ),
+            isExpanded: true,
+            hint: const Text('자녀', style: TextStyle(fontSize: 14, fontFamily: 'GmarketSans')),
+            value: selectedChild,
+            items: childrenItems
+                .map<DropdownMenuItem<String>>((item) => DropdownMenuItem<String>(
+              value: item,
+              child: Text(item, style: const TextStyle(fontSize: 14, fontFamily: 'GmarketSans')),
+            ))
+                .toList(),
+            onChanged: (value) {
+              Child selected =
+              childrenData.firstWhere((child) => child.username == value);
+              updateChildSelection(selected);
+            },
+            dropdownStyleData: DropdownStyleData(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+              ),
             ),
           ),
         ),
+        SizedBox(width: 150,)
       ],
     );
   }
+
+
+
+
 
   Widget buildEmptyGoals() {
     return Expanded(
@@ -193,10 +202,10 @@ class _GoalState extends State<Goal> {
           children: [
             const Text(
               "목표가 설정되지 않았습니다.",
-              style: TextStyle(fontSize: 20),
+              style: TextStyle(fontSize: 20, fontFamily: 'GmarketSans'),
             ),
-            const Text("자녀의 안전 보행을 위해", style: TextStyle(fontSize: 20)),
-            const Text("보행 목표를 설정해주세요!", style: TextStyle(fontSize: 20)),
+            const Text("자녀의 안전 보행을 위해", style: TextStyle(fontSize: 20, fontFamily: 'GmarketSans')),
+            const Text("보행 목표를 설정해주세요!", style: TextStyle(fontSize: 20, fontFamily: 'GmarketSans')),
             const SizedBox(
               height: 10,
             ),
