628565c9f15cff49b4278d09e7090ffa0caf311f
hluuy <dudwns1289@gmail.com>
2024-05-18 14:04:55 +0900

    Fix : css 수정

--- FE/icecream/lib/auth/screen/fork_page.dart
+++ FE/icecream/lib/auth/screen/fork_page.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:go_router/go_router.dart';
-import 'package:icecream/auth/service/user_service.dart';
 import 'package:icecream/com/widget/default_layout.dart';
-import 'package:icecream/provider/user_provider.dart';
 import 'package:icecream/setting/widget/custom_elevated_button.dart';
-import 'package:provider/provider.dart';
 
 class ForkPage extends StatefulWidget {
   const ForkPage({super.key});
@@ -15,21 +11,10 @@ class ForkPage extends StatefulWidget {
 }
 
 class _ForkPageState extends State<ForkPage> {
-  final UserService _userService = UserService();
 
   @override
   void initState() {
     super.initState();
-  }
-
-  // 자동 로그인
-  Future<void> _autoLogin() async {
-    final userProvider = Provider.of<UserProvider>(context, listen: false);
-    try {
-      await _userService.autoLogin(userProvider);
-    } catch (e) {
-      debugPrint('Auto-login failed: $e');
-    }
   }
 
   @override
@@ -37,39 +22,61 @@ class _ForkPageState extends State<ForkPage> {
     return DefaultLayout(
       title: '',
       child: Center(
-        child: Column(
-          children: [
-            CustomElevatedButton(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0), // 패딩 조정
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center, // 중앙 정렬
+            crossAxisAlignment: CrossAxisAlignment.stretch, // 너비 최대화
+            children: [
+              const Text(
+                '저희 서비스는 처음이신가요?',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'GmarketSans',
+                  fontWeight: FontWeight.w500,
+                  fontSize: 24.0,
+                ),
+              ),
+              const SizedBox(height: 30.0), // 텍스트와 버튼 사이의 여백 추가
+              CustomElevatedButton(
                 onPressed: () {
                   context.goNamed('signup');
                 },
-                child: '부모'),
-            SizedBox(
-              height: 10.0,
-            ),
-            CustomElevatedButton(
+                child: '부모이신가요?',
+                height: 150, // 높이 조정
+                image: 'asset/img/parents.png', // 부모 아이콘 이미지 경로 추가
+              ),
+              const SizedBox(height: 20.0), // 간격 증가
+              CustomElevatedButton(
                 onPressed: () {
                   context.goNamed('c_qrcode');
                 },
-                child: '자녀'),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text('로그인할꺼야?'),
-            SizedBox(
-              height: 10.0,
-            ),
-            CustomElevatedButton(
+                child: '자녀이신가요?',
+                height: 150, // 높이 조정
+                image: 'asset/img/children.png', // 자녀 아이콘 이미지 경로 추가
+              ),
+              const SizedBox(height: 30.0), // 버튼과 텍스트 사이의 여백 증가
+              const Text(
+                '계정이 이미 있으신가요? ',
+                textAlign: TextAlign.center, // 중앙 정렬
+                style: TextStyle(
+                  fontFamily: 'GmarketSans', 
+                  fontWeight: FontWeight.w400,
+                  fontSize: 16.0, // 글꼴 크기 증가
+                ),
+              ),
+              const SizedBox(height: 20.0),
+              CustomElevatedButton(
                 onPressed: () {
                   context.goNamed('p_login');
                 },
-                child: '로그인 버튼'),
-          ],
+                child: '로그인',
+                height: 70,
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
-//     );
-//   }
-// }
