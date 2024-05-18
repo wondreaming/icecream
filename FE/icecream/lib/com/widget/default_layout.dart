9c63bceb2834fc860dc2e4d0002470adea3cfc01
hluuy <dudwns1289@gmail.com>
2024-05-19 05:59:40 +0900

    Fix : snackBar 알림 toast로 수정 & fork_page appBar 삭제

--- FE/icecream/lib/com/widget/default_layout.dart
+++ FE/icecream/lib/com/widget/default_layout.dart
@@ -3,23 +3,25 @@ import 'package:icecream/com/const/color.dart';
 
 class DefaultLayout extends StatelessWidget {
   final Widget child;
-  final String title;
+  final String? title;
   final List<Widget>? action;
   final EdgeInsets padding; // 패딩값 추가
   final bool isMap;
   final Widget? floatingActionButton;
   final bool? automaticallyImplyLeading;
   final bool isSetting;
+  final bool isAppbar;
   const DefaultLayout({
     super.key,
     required this.child,
-    required this.title,
+    this.title,
     this.action,
     this.padding = const EdgeInsets.symmetric(horizontal: 16.0), // 기본값 설정
     this.isMap = false,
     this.floatingActionButton,
     this.automaticallyImplyLeading = true,
     this.isSetting = false,
+    this.isAppbar = true,
   });
 
   @override
@@ -27,12 +29,12 @@ class DefaultLayout extends StatelessWidget {
     return Scaffold(
       resizeToAvoidBottomInset: false, // 키보드 올라왔을 때, 영역 보존
       backgroundColor: AppColors.background_color,
-      appBar: AppBar(
+      appBar: isAppbar ? AppBar(
         // centerTitle: true,
         automaticallyImplyLeading: automaticallyImplyLeading!,
         scrolledUnderElevation: 0,
         title: Text(
-          title,
+          title!,
           style: TextStyle(
             fontSize: 30,
             fontWeight: FontWeight.w500,
@@ -42,8 +44,8 @@ class DefaultLayout extends StatelessWidget {
         actions: action,
         centerTitle: false, // 제목을 왼쪽으로 정렬
         toolbarHeight: 80.0, // 앱바의 높이를 조정
-        titleSpacing: isSetting? 3.0 : 28.0, // 제목의 시작 부분에 공간
-      ),
+        titleSpacing: isSetting ? 3.0 : 28.0, // 제목의 시작 부분에 공간
+      ) : null,
       body: Padding(
         // 패딩값 사용
         padding: isMap
