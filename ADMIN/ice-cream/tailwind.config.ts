fbea4dafd1efb3603676eee103979b4a5a33cfd1
심상익 <tkddlrtla@naver.com>
2024-05-20 00:39:17 +0900

    Fix : 과속 알림 표시 최근 10개만 나오게 수정 및 애니메이션 효과 적용, 폰트 조정

--- ADMIN/ice-cream/tailwind.config.ts
+++ ADMIN/ice-cream/tailwind.config.ts
@@ -14,6 +14,15 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      keyframes: {
+        blink: {
+          "0%, 100%": { opacity: "1" },
+          "50%": { opacity: "0" },
+        },
+      },
+      animation: {
+        blink: "blink 1s linear infinite",
+      },
     },
   },
   plugins: [],
